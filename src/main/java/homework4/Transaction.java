2515f701bcffb45617e69dda6dfd8218670362a4
BatmanovSergeydotnet <xoolly@mail.ru>
2023-11-30 02:15:21 +0300

    Commit HW4.2

--- src/main/java/homework4/Transaction.java
+++ src/main/java/homework4/Transaction.java
@@ -1,24 +1,24 @@
 package homework4;
 
 public class Transaction {
-    private final Account account1;
-    private final Account account2;
+    private final Account moneyFrom;
+    private final Account moneyTo;
     private final double money;
 
-    public Transaction(Account account1, Account account2, double money) {
-        this.account1 = account1;
-        this.account2 = account2;
+    public Transaction(Account moneyFrom, Account moneyTo, double money) {
+        this.moneyFrom = moneyFrom;
+        this.moneyTo = moneyTo;
         this.money = money;
     }
 
     public void transferMoney()
             throws InsufficientFundsException {
-        if (money > account1.getBalans()) {
+        if (money > moneyFrom.getBalans()) {
             throw new InsufficientFundsException(String.format("Сумма перевода превышает баланс счёта!!!\n " +
-                    "Баланс равен: %.2f!\n",account1.getBalans()));
+                    "Баланс равен: %.2f!\n",moneyFrom.getBalans()));
         }
-        account1.withdrawMoney(money);
-        account2.addAccount(money);
+        moneyFrom.withdrawMoney(money);
+        moneyTo.addAccount(money);
 
     }
 
